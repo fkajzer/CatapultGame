e57016e0e61dc4ddbf0b7c5bc10fe6be005795e8
fkajzer <filipkajzer@gmail.com>
2020-06-23 23:13:34 +0200

    launch bird!

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+
+    private void Awake() {
+        _initialPosition = transform.position;
+    }
+
+    private void Update() {
+        if (transform.position.y > 10) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnMouseDown() {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp() {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 1000);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    private void OnMouseDrag() {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = newPosition;
+    }
+}
